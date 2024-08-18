723fb54ce4e6d758ecd49770cabd21b4cb16f62a
Omar Ahmed <omarahmed19991@outlook.com>
2024-08-18 12:18:57 +0300

    Move fetchUser function to the context file

--- client/src/context/context.jsx
+++ client/src/context/context.jsx
@@ -1,16 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 export const Context = createContext(null);
 
 const ContextProvider = (props) => {
 
+    const [loggedInUser, setLoggedInUser] = useState({});
+
     const serverLink = "http://localhost:3000/";
     const navigate = useNavigate();
+    async function fetchUser() {
+        const token = localStorage.getItem('token');
+        if(!token){
+            alert('Please login');
+            navigate('/login');
+            return;
+        }
+        try {
+            const response = await axios.get(serverLink+'api/user', {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            });
+            setLoggedInUser(response.data.user);
+        } catch (error) {
+            alert('Failed to fetch user data. Please log in again.');
+            console.log(error);
+            navigate('/login');
+        }
+    }
 
     const contextValue = {
         serverLink,
         navigate,
+        loggedInUser,
+        fetchUser,
     };
 
     return (
--- client/src/pages/Dashboard/dashboard.jsx
+++ client/src/pages/Dashboard/dashboard.jsx
@@ -4,42 +4,16 @@ import axios from 'axios';
 
 const Dashboard = () => {
 
-  const [user, setUser] = useState({});
-  const { navigate, serverLink } = useContext(Context);
+  const { loggedInUser, fetchUser } = useContext(Context);
 
   useEffect(() => {
-    async function fetchUser() {
-      const token = localStorage.getItem('token');
-      if(!token){
-        alert('Please login');
-        navigate('/login');
-        return;
-      }
-      try {
-        const response = await axios.get(serverLink+'api/user', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        });
-        console.log('response: ', response);
-        setUser(response.data.user);
-      } catch (error) {
-        alert('Failed to fetch user data. Please log in again.');
-        console.log(error);
-        navigate('/login');
-      }
-    }
-
     fetchUser();
   }, []);
 
 
   return (
     <div>
-      <h1>Welcome to the Dashboard!</h1>
-      <p>{user.username}</p>
-      <p></p>
-      <p></p>
+      
     </div>
   )
 }
